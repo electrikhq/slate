cc4cd3f08200640bb309b3e184c83bfa00d1bcaa
neerajsohal <hello@neerajkumar.name>
2021-12-17 23:53:30 +0530

    added badges

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,21 +1,23 @@
 @props([
-    'color' => 'primary',
+    'color' => "primary",
 	'href' => null,
-	'target' => 'null',
+	'target' => null,
+	'id' => null,
 ])
 
 <a {{ $attributes
 		->class([
 			'focus:underline focus:outline-none hover:underline hover:outline-none',
 			'text-primary-600 hover:text-primary-700' => ($color == "primary"),
+			'text-secondary-600 hover:text-secondary-700' => ($color == "secondary"),
 			'text-success-600 hover:text-success-700' => ($color == "success"),
 			'text-danger-600 hover:text-danger-700' => ($color == "danger"),
 			'text-warning-600 hover:text-warning-700' => ($color == "warning"),
 			'text-info-600 hover:text-info-700' => ($color == "info"),
-			$attributes->merge()['class'],
 		])
 	}}
 
+	@if ($id) href="{{ $id }}" @endif
 	@if ($href) href="{{ $href }}" @endif
 	@if ($target) target="{{ $target }}" @endif
 >
