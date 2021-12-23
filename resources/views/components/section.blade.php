899695e8cad2657412f3eb98d9d8bd7ac2271791
neerajsohal <hello@neerajkumar.name>
2021-12-23 12:47:01 +0530

    created section component

---
+++ resources/views/components/section.blade.php
@@ -0,0 +1,24 @@
+<div class="grid overflow-hidden grid-cols-3 grid-rows-1 gap-2 my-4">
+	@if($left)
+	<div {{ 
+		$attributes->class([
+			"min-w-full min-h-full rounded col-span-1 px-5 py-4",
+			"shadow-md bg-white rounded-md" => !$left->attributes->get('transparent')
+		])
+	}}
+	>
+		{{ $left }}	
+	</div>
+	@endif
+	@if($right)
+	<div {{ 
+		$attributes->class([
+			"min-w-full min-h-full rounded col-span-2 px-5 py-4",
+			"shadow-md bg-white rounded-md" => !$right->attributes->get('transparent')
+		])
+	}}
+	>
+		{{ $right }}	
+	</div>
+	@endif
+</div>
