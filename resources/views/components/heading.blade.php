af6aeca191f6cade2b8da9749f44db29b3c74d00
neerajsohal <hello@neerajkumar.name>
2021-12-24 21:22:11 +0530

    restored font weights for headings

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -10,14 +10,24 @@
 @if($tag == 'h1')
 <h1 {{
 	$attributes->class([
-		'prose dark:prose-invert prose-2xl'
+		'prose dark:prose-invert prose-2xl',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
 	])
 }}>{{ $slot }}</h1>
 @endif
 @if($tag == 'h2')
 <h2 {{
 	$attributes->class([
-		'prose dark:prose-invert prose-xl'
+		'prose dark:prose-invert prose-xl',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
 
 	])
 }}>{{ $slot }}</h2>
@@ -25,7 +35,12 @@
 @if($tag == 'h3')
 <h3 {{
 	$attributes->class([
-		'prose dark:prose-invert prose-lg'
+		'prose dark:prose-invert prose-lg',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
 
 	])
 }}>{{ $slot }}</h3>
@@ -33,7 +48,12 @@
 @if($tag == 'h4')
 <h4 {{
 	$attributes->class([
-		'prose dark:prose-invert prose-base'
+		'prose dark:prose-invert prose-base',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
 
 	])
 }}>{{ $slot }}</h4>
@@ -41,7 +61,12 @@
 @if($tag == 'h5')
 <h5 {{
 	$attributes->class([
-		'prose dark:prose-invert prose-sm'
+		'prose dark:prose-invert prose-sm',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
 
 	])
 }}>{{ $slot }}</h5>
@@ -49,7 +74,12 @@
 @if($tag == 'h6')
 <h6 {{
 	$attributes->class([
-		'prose dark:prose-invert prose-sm'
+		'prose dark:prose-invert prose-sm',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
 
 	])
 }}>{{ $slot }}</h6>
