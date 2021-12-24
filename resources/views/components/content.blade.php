5d8c69c54af0a001475e450f33a2870c53037e19
neerajsohal <hello@neerajkumar.name>
2021-12-24 23:34:24 +0530

    .

---
+++ resources/views/components/content.blade.php
@@ -0,0 +1,7 @@
+<div {{
+	$attributes->class([
+		'prose prose-base dark:prose-invert max-w-none'
+	])
+}}>
+	{{ $slot }}
+</div>
---
+++ resources/views/components/heading.blade.php
@@ -0,0 +1,86 @@
+@props([
+	'tag'=>'h1',
+	'fontThin' => null,
+	'fontRegular' => null,
+	'fontMedium' => null,
+	'fontBold' => null,
+	'fontBlack' => null,
+])
+
+@if($tag == 'h1')
+<h1 {{
+	$attributes->class([
+		'prose dark:prose-invert prose-2xl max-w-none',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
+	])
+}}>{{ $slot }}</h1>
+@endif
+@if($tag == 'h2')
+<h2 {{
+	$attributes->class([
+		'prose dark:prose-invert prose-xl max-w-none',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
+
+	])
+}}>{{ $slot }}</h2>
+@endif
+@if($tag == 'h3')
+<h3 {{
+	$attributes->class([
+		'prose dark:prose-invert prose-lg max-w-none',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
+
+	])
+}}>{{ $slot }}</h3>
+@endif
+@if($tag == 'h4')
+<h4 {{
+	$attributes->class([
+		'prose dark:prose-invert prose-base max-w-none',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
+
+	])
+}}>{{ $slot }}</h4>
+@endif
+@if($tag == 'h5')
+<h5 {{
+	$attributes->class([
+		'prose dark:prose-invert prose-sm max-w-none',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
+
+	])
+}}>{{ $slot }}</h5>
+@endif
+@if($tag == 'h6')
+<h6 {{
+	$attributes->class([
+		'prose dark:prose-invert prose-sm max-w-none',
+		'font-thin' => $fontThin,
+		'font-regular' => $fontRegular,
+		'font-medium' => $fontMedium,
+		'font-bold' => $fontBold,
+		'font-black' => $fontBlack,
+
+	])
+}}>{{ $slot }}</h6>
+@endif
