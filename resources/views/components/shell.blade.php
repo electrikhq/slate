d26627ccd6b30832735b4b980f691beb6936098a
neerajsohal <hello@neerajkumar.name>
2021-12-28 18:25:11 +0530

    refactored shell

--- resources/views/components/shell.blade.php
+++ resources/views/components/shell.blade.php
@@ -1,57 +1,14 @@
-@props([
-	'id' => null,
-	'sidebar' => null,
-	'brand' => null,
-	'link' => null
-])
+<div class="flex h-screen overflow-hidden">
+	
+	@if($sidebar)
+		{{ $sidebar }}
+	@endif
 
-<div 
-	@if ($id) {{ $id }} @endif
->
-
-	<div class="flex flex-col md:flex-row ">
-
-		<aside
-			class="w-full md:w-64 md:min-h-screen bg-gray-100 dark:bg-gray-800 shadow-md	"
-			x-data="{ isOpen: false }" 
-		>
-		<div class="flex items-center justify-between p-4 h-16 bg-white dark:bg-gray-900">
-			@if('brand')
-			<x-slate::brand
-				:brand="$brand"
-				icon="carbon-dashboard"
-			/>
-			@endif
-					
-			<div class="flex md:hidden">
-				<button type="button" @click="isOpen = !isOpen" class="">
-					<x-carbon-menu class="w-7 h-7 focus:outline-none focus:text-gray-700 text-gray-900 dark:text-gray-100" />
-				</button>
-			</div>
+	<div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+		<div class="flex" x-data="{}">
+			<x-slate::icon icon="carbon-menu" data-target="slate-sidebar" @click="$dispatch('toggle-sidebar', $el.dataset.target)" class="shrink-0 text-gray-600" />
 		</div>
-			<div class="px-2 py-6 md:block" :class="isOpen? 'block': 'hidden'">
-				{!! $sidebar !!}
-			</div>
-		</aside>
-
-		<div class="w-full md:flex-1">
-			<nav class="hidden md:flex justify-between items-center p-4 h-16 bg-white dark:bg-gray-900">
-
-				<x-slate::navigation>
-					<x-slot name="left">
-						{{ $right }}
-					</x-slot>
-					<x-slot name="right">
-						{{ $left }}
-					</x-slot>
-				</x-slate::navigation>
-			</nav>
-			<main>
-
-				{{ $slot }}
-
-			</main>
-			
-		</div>
+		{{ $slot }}
 	</div>
+
 </div>
