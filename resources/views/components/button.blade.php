9cd7f34306e7c83f6d7d4b5a796f24a8bd576f24
neerajsohal <hello@neerajkumar.name>
2022-01-04 17:24:32 +0530

    improved ui. added space, form, and alert components

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -4,8 +4,10 @@
     'type' => 'text',
 	'color' => 'primary',
 	'value' => null,
-	'size' => 'md',
-	'outline' => null,
+	'size' => 'sm',
+	'outlined' => null,
+	'fullWidth' => null,
+	'thick' => null,
 ])
 
 @php
@@ -15,19 +17,22 @@
 	type="{{ $type ??= 'button' }}" 
 	{{ $attributes
 		->class([
-			"inline-flex uppercase shadow-md justify-center py-2 px-4 border border-transparent font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2",
-			'bg-primary-600 hover:bg-primary-700 focus:bg-primary-700' => ($color == "primary" && !$outline),
-			'bg-secondary-600 hover:bg-secondary-700 focus:bg-secondary-700' => ($color == "secondary" && !$outline),
-			'bg-success-600 hover:bg-success-700 focus:bg-success-700' => ($color == "success" && !$outline),
-			'bg-danger-600 hover:bg-danger-700 focus:bg-danger-700' => ($color == "danger" && !$outline),
-			'bg-warning-600 hover:bg-warning-700 focus:bg-warning-700' => ($color == "warning" && !$outline),
-			'bg-info-600 hover:bg-info-700 focus:bg-info-700' => ($color == "info" && !$outline),
-			'bg-transparent border border-primary-600 hover:border-primary-900 focus:border-primary-700 text-primary-700 hover:text-primary-900' => ($color == "primary" && $outline),
-			'bg-transparent border border-secondary-600 hover:border-secondary-900 focus:border-secondary-700 text-secondary-700 hover:text-secondary-900' => ($color == "secondary" && $outline),
-			'bg-transparent border border-success-600 hover:border-success-900 focus:border-success-700 text-success-700 hover:text-success-900' => ($color == "success" && $outline),
-			'bg-transparent border border-danger-600 hover:border-danger-900 focus:border-danger-700 text-danger-700 hover:text-danger-900' => ($color == "danger" && $outline),
-			'bg-transparent border border-warning-600 hover:border-warning-900 focus:border-warning-700 text-warning-700 hover:text-warning-900' => ($color == "warning" && $outline),
-			'bg-transparent border border-info-600 hover:border-info-900 focus:border-info-700 text-info-700 hover:text-info-900' => ($color == "info" && $outline),
+			"uppercase shadow-md justify-center py-2 px-4 border border-transparent font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 tracking-widest",
+			"inline-flex" => (!$fullWidth),
+			"inline-block w-full" => ($fullWidth),
+			'bg-primary-600 hover:bg-primary-700 focus:bg-primary-700' => ($color == "primary" && !$outlined),
+			'bg-secondary-600 hover:bg-secondary-700 focus:bg-secondary-700' => ($color == "secondary" && !$outlined),
+			'bg-success-600 hover:bg-success-700 focus:bg-success-700' => ($color == "success" && !$outlined),
+			'bg-danger-600 hover:bg-danger-700 focus:bg-danger-700' => ($color == "danger" && !$outlined),
+			'bg-warning-600 hover:bg-warning-700 focus:bg-warning-700' => ($color == "warning" && !$outlined),
+			'bg-info-600 hover:bg-info-700 focus:bg-info-700' => ($color == "info" && !$outlined),
+			'bg-transparent border border-primary-600 hover:border-primary-900 focus:border-primary-700 text-primary-700 hover:text-primary-900' => ($color == "primary" && $outlined),
+			'bg-transparent border border-secondary-600 hover:border-secondary-900 focus:border-secondary-700 text-secondary-700 hover:text-secondary-900' => ($color == "secondary" && $outlined),
+			'bg-transparent border border-success-600 hover:border-success-900 focus:border-success-700 text-success-700 hover:text-success-900' => ($color == "success" && $outlined),
+			'bg-transparent border border-danger-600 hover:border-danger-900 focus:border-danger-700 text-danger-700 hover:text-danger-900' => ($color == "danger" && $outlined),
+			'bg-transparent border border-warning-600 hover:border-warning-900 focus:border-warning-700 text-warning-700 hover:text-warning-900' => ($color == "warning" && $outlined),
+			'bg-transparent border border-info-600 hover:border-info-900 focus:border-info-700 text-info-700 hover:text-info-900' => ($color == "info" && $outlined),
+
 			'text-xs' => ($size == 'xs'),
 			'text-sm' => ($size == 'sm'),
 			'text-md' => ($size == 'md'),
@@ -38,6 +43,7 @@
 			'text-5xl' => ($size == '5xl'),
 			'text-6xl' => ($size == '6xl'),
 			'text-7xl' => ($size == '7xl'),
+			'border-2' => ($thick),
 			])
 	}}
 
--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -5,10 +5,10 @@
 
 <div {{ 
 		$attributes->class([
-			"grid overflow-hidden grid-rows-1  my-4",
+			"grid overflow-hidden grid-rows-1 my-4",
 			"grid-cols-1" => ($attributes->get('cols') == 1),
 			"grid-cols-3" => ($attributes->get('cols') != 1),
-			"shadow-md bg-white rounded-md dark:bg-gray-900" => !$attributes->get('transparent')
+			"shadow-md bg-white rounded-md dark:bg-gray-900 dark:text-white" => !$attributes->get('transparent')
 		])
 	}}
 >
@@ -18,7 +18,7 @@
 	<div {{ 
 		$attributes->class([
 			"rounded col-span-1 mx-4 py-4",
-			"shadow-md bg-white rounded-md dark:bg-gray-900 px-4" => !$left->attributes->get('transparent')
+			"shadow-md bg-white rounded-md dark:bg-gray-900 px-4 dark:text-white" => !$left->attributes->get('transparent')
 		])
 	}}
 	>
@@ -29,7 +29,7 @@
 	<div {{ 
 		$attributes->class([
 			"rounded col-span-2 mx-4 py-4",
-			"shadow-md bg-white rounded-md dark:bg-gray-900 px-4" => !$right->attributes->get('transparent')
+			"shadow-md bg-white rounded-md dark:bg-gray-900 px-4 dark:text-white" => !$right->attributes->get('transparent')
 		])
 	}}
 	>
@@ -40,7 +40,7 @@
 @else 
 <div {{ 
 		$attributes->class([
-			"min-w-full min-h-full rounded col-span-1 px-5 py-4",
+			"min-w-full min-h-full rounded col-span-1 px-5 py-4 dark:text-white",
 		])
 	}}
 >
