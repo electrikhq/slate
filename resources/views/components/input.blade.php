1ffa3a751053cf9f10a297e9a6da09ced26c9ad0
neerajsohal <hello@neerajkumar.name>
2024-08-13 14:07:57 +0530

    enhanced border on shell

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -16,7 +16,7 @@
 		<label
 			for="{{ $name }}"
 			{{ $attributes->class([
-				'block font-medium cursor-pointer text-gray-700 dark:text-gray-500',
+				'block font-medium cursor-pointer text-gray-900 dark:text-gray-100',
 				'text-danger-600' => ($name && $errors->has($name)),
 			]) }}"
 		>
@@ -29,18 +29,18 @@
         'value' => old($name, $value),
         'placeholder' => $placeholder,
         'id' => $name,
-        'class' => 'block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:focus:ring-gray-500 dark:focus:border-gray-500' .
+        'class' => 'block w-full px-3 py-2 border rounded-md shadow-xs placeholder-gray-400 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:focus:ring-gray-500 dark:focus:border-gray-500' .
                   ($errors->has($name) ? ' border-red-600' : ' border-gray-300') .
-                  ($disabled ? ' bg-gray-100' : '') .
-                  ($readonly ? ' bg-gray-50' : '') .
-                  ' dark:bg-gray-950 dark:text-white dark:border-gray-600 dark:placeholder-gray-800'
+                  ($disabled ? ' bg-gray-400' : '') .
+                  ($readonly ? ' bg-gray-100' : '') .
+                  ' dark:bg-black dark:text-white dark:border-gray-900 dark:placeholder-gray-600'
         ]) }}
         {{ $required ? 'required' : '' }}
         {{ $readonly ? 'readonly' : '' }}
         {{ $disabled ? 'disabled' : '' }}
     />
     @if($helpText)
-        <p class="mt-1 text-sm text-gray-500 dark:text-gray-800">{{ $helpText }}</p>
+        <p class="mt-1 text-sm font-light text-gray-800 dark:text-gray-300">{{ $helpText }}</p>
     @endif
     @if($error)
         <p class="mt-1 text-sm text-red-600">{{ $error }}</p>
