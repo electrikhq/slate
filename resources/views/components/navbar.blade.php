7228a5f3c9f3424c8390f65b028888dd56b524c4
neerajsohal <hello@neerajkumar.name>
2024-08-18 16:18:18 +0530

    updated flex properties to make navbar and shell more customizable

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav 
     {{ $attributes->class([
-            'border-b-[1px] justify-between py-3 items-center space-x-8 max-w-full w-full px-2 bg-white text-gray-900 dark:bg-black dark:text-gray-100 dark:border-b-[1px] dark:border-gray-900/60'
+            'border-b-[1px] flex py-3 items-center space-x-8 max-w-full w-full px-2 bg-white text-gray-900 dark:bg-black dark:text-gray-100 dark:border-b-[1px] dark:border-gray-900/60'
         ]) 
     }}"
 >
