8389a4c3f0ec59ac8a0c7585a26c2e4dcc9ebe26
neerajsohal <hello@neerajkumar.name>
2024-08-19 19:35:44 +0530

    streamlined card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -11,8 +11,8 @@
 // Define size classes with reduced padding
 $sizes = [
     'sm' => 'p-2 text-sm',
-    'md' => 'p-4 text-base', // Reduced padding for default size
-    'lg' => 'p-6 text-lg',
+    'md' => 'p-2 text-base', // Reduced padding for default size
+    'lg' => 'p-2 text-lg',
 ];
 
 $sizeClass = $sizes[$size] ?? $sizes['md'];
@@ -33,14 +33,14 @@ if (in_array($color, ['black', 'white'])) {
         : "bg-{$color}-500 text-white dark:bg-{$color}-700 dark:text-gray-200";
 }
 
-$roundedClass = 'rounded-lg shadow-md'; // Consistent rounded corners and shadow for cards
+$roundedClass = 'rounded-lg'; // Consistent rounded corners and shadow for cards
 @endphp
 
 <div {{ $attributes->merge(['class' => "$sizeClass $colorClass $roundedClass"]) }}>
     @if ($header)
-        <div class="flex items-center border-b border-gray-200 dark:border-gray-700 px-4 py-2">
+        <div class="flex items-center border-b border-gray-200 dark:border-gray-700 px-2 py-2">
             @if ($icon)
-                <x-slate::icon :icon="$icon" size="md" class="mr-2"/>
+                <x-slate::icon :icon="$icon" size="sm" class="mr-2" />
             @endif
             <div class="flex-1">
                 {{ $header }}
@@ -48,12 +48,12 @@ $roundedClass = 'rounded-lg shadow-md'; // Consistent rounded corners and shadow
         </div>
     @endif
 
-    <div class="px-4 py-2">
+    <div class="px-2 py-4">
         {{ $slot }}
     </div>
 
     @if ($footer)
-        <div class="border-t border-gray-200 dark:border-gray-700 px-4 py-2">
+        <div class="border-t border-gray-200 dark:border-gray-700 px-2 py-2">
             {{ $footer }}
         </div>
     @endif
