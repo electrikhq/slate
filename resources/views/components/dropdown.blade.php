2b5f20333bad862f0609fc0c694a7ed606d2b760
neerajsohal <hello@neerajkumar.name>
2024-08-19 19:25:46 +0530

    added status component, updated dropdown

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,17 +1,26 @@
 @props([
     'label' => null, // The label for the dropdown trigger
-    'size' => 'md', // Size of the dropdown: 'sm', 'md', 'lg'
+    'size' => 'md', // Size of the dropdown: 'xs', 'sm', 'md', 'lg', 'xl'
     'color' => 'black', // Color of the dropdown trigger button
     'icon' => null, // Optional icon for the dropdown trigger
-    'position' => 'bottom-right', // Position of the dropdown menu: 'bottom-right', 'bottom-left', 'top-right', 'top-left'
+    'position' => 'bottom-left', // Position of the dropdown menu: 'bottom-right', 'bottom-left', 'top-right', 'top-left'
     'disabled' => false, // Disable the dropdown
+    'hasShadow' => true, // Option to have shadow
 ])
 
 @php
 $sizes = [
-    'sm' => 'text-sm py-1.5 px-3',
-    'md' => 'text-base py-2 px-4', // Default size
-    'lg' => 'text-lg py-2.5 px-5',
+    'xs' => 'text-xs py-1 px-2',
+    'sm' => 'text-sm py-1 px-2',
+    'md' => 'text-base py-1.5 px-3', // Default size
+    'lg' => 'text-lg py-2 px-4',
+];
+
+$iconSizes = [
+    'xs' => 'xs',
+    'sm' => 'xs',
+    'md' => 'sm',
+    'lg' => 'md',
 ];
 
 $sizeClass = $sizes[$size] ?? $sizes['md'];
@@ -30,27 +39,31 @@ if (in_array($color, ['black', 'white'])) {
 
 // Determine the positioning classes for the dropdown menu
 $positionClasses = match ($position) {
-    'bottom-left' => 'left-0 top-full mt-2',
-    'top-right' => 'right-0 bottom-full mb-2',
-    'top-left' => 'left-0 bottom-full mb-2',
-    default => 'right-0 top-full mt-2', // Default to bottom-right
+    'bottom-left' => 'left-0 top-full mt-1.5',
+    'top-right' => 'right-0 bottom-full mb-1.5',
+    'top-left' => 'left-0 bottom-full mb-1.5',
+    default => 'right-0 top-full mt-1.5', // Default to bottom-right
 };
+
+// Handle shadow based on hasShadow prop
+$shadowClass = $hasShadow ? 'shadow-lg' : '';
 @endphp
 
 <div x-data="{ open: false }" class="relative inline-block text-left">
     <!-- Trigger Button -->
     <button @click="open = !open" :class="{ 'opacity-50 cursor-not-allowed': {{ $disabled ? 'true' : 'false' }} }"
             {{ $disabled ? 'disabled' : '' }}
-            class="inline-flex items-center justify-between {{ $sizeClass }} {{ $colorClass }} rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-gray-700">
+            class="inline-flex items-center justify-between {{ $sizeClass }} {{ $colorClass }} rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-gray-700">
         @if($icon)
-            <x-slate::icon :icon="$icon" size="sm" class="mr-2" color="{{ $iconColor }}"/>
+            <x-slate::icon :icon="$icon" :size="$iconSizes[$size]" color="{{ $iconColor }}" class="mr-1" />
         @endif
         {{ $label }}
-        <x-slate::icon icon="carbon-chevron-down" size="sm" class="ml-2 {{ $iconColor }}"/>
+        <x-slate::icon icon="carbon-chevron-down" :size="$iconSizes[$size]" color="{{ $iconColor }}" />
     </button>
 
     <!-- Dropdown Menu -->
-    <div x-show="open" @click.away="open = false" class="absolute z-10 {{ $positionClasses }} w-auto min-w-full bg-white dark:bg-black border border-gray-300 dark:border-gray-700 rounded-md shadow-lg overflow-hidden">
+    <div x-show="open" @click.away="open = false" 
+        class="absolute z-10 {{ $positionClasses }} min-w-[100%] bg-white dark:bg-black border border-gray-300 dark:border-gray-700 rounded-md {{ $shadowClass }} overflow-hidden">
         <div class="py-1" role="menu">
             {{ $slot }}
         </div>
---
+++ resources/views/components/status.blade.php
@@ -0,0 +1,45 @@
+@props([
+    'size' => 'md', // Size of the status dot: 'xs', 'sm', 'md', 'lg', 'xl'
+    'color' => 'green', // Color of the status dot, e.g., 'green', 'red', 'blue', 'yellow', 'black', 'white'
+    'outlined' => false, // Outlined style
+    'label' => null, // Optional label next to the status dot
+])
+
+@php
+// Define size classes
+$sizes = [
+    'xs' => 'h-2 w-2',
+    'sm' => 'h-3 w-3',
+    'md' => 'h-4 w-4', // Default size
+    'lg' => 'h-5 w-5',
+    'xl' => 'h-6 w-6',
+];
+
+$sizeClass = $sizes[$size] ?? $sizes['md']; // Fallback to 'md' if size is not provided or invalid
+
+// Handle black and white colors explicitly
+if (in_array($color, ['black', 'white'])) {
+    $colorClass = $outlined
+        ? ($color === 'black'
+            ? 'bg-transparent border border-black text-black dark:bg-transparent dark:border-white dark:text-white'
+            : 'bg-transparent border border-black text-black dark:bg-transparent dark:border-white dark:text-white')
+        : ($color === 'black'
+            ? 'bg-black text-white dark:bg-white dark:text-black'
+            : 'bg-white text-black dark:bg-black dark:text-white');
+} else {
+    // Handle other colors dynamically
+    $colorClass = $outlined
+        ? "bg-transparent border border-{$color}-500 text-{$color}-500 dark:border-{$color}-400 dark:text-{$color}-400"
+        : "bg-{$color}-500 text-white dark:bg-{$color}-700 dark:text-gray-200";
+}
+
+// Combine all classes
+$classes = implode(' ', [$sizeClass, $colorClass, 'inline-block rounded-full']);
+@endphp
+
+<div class="flex items-center">
+    <span {{ $attributes->merge(['class' => $classes]) }}></span>
+    @if ($label)
+        <span class="ml-2 text-sm font-medium text-gray-700 dark:text-gray-300">{{ $label }}</span>
+    @endif
+</div>
